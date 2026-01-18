5e3547a0e2ccf9064afb8e0aba6a711a6c14f961
Showrav-tech <showravc85@gmail.com>
2026-01-18 13:07:37 +0600

    Add all code files

---
+++ Implementation/1454A.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        for (int i = 1; i <= n; i++)
+        {
+            if (i < n)
+                cout << i + 1 << " ";
+            else
+                cout << 1 << "\n";
+        }
+    }
+    return 0;
+}

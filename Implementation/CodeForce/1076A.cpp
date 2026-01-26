c3a14b1aede305f4fc2c0a73b545b5142683bf53
Showrav-tech <showravc85@gmail.com>
2026-01-26 20:11:12 +0600

    Add CF + LC solutions (Day 7)

---
+++ Implementation/CodeForce/1076A.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, s, x;
+        cin >> n >> s >> x;
+
+        vector<int> a(n);
+        int sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+            sum += a[i];
+        }
+
+        if (s >= sum && (s - sum) % x == 0)
+        {
+            cout << "YES\n";
+        }
+        else
+        {
+            cout << "NO\n";
+        }
+    }
+    return 0;
+}

b18188f5d0152618c9863164940bcc526f3bb9b7
Showrav-tech <showravc85@gmail.com>
2026-01-27 21:50:40 +0600

    Add CF + LC solutions (DAY 8)

---
+++ Implementation/CodeForce/34B.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end());
+    int sum = 0;
+    for (int i = 0; i < m; i++)
+    {
+        if (a[i] < 0)
+            sum += -a[i];
+        else
+            break;
+    }
+    cout << sum << endl;
+    return 0;
+}
